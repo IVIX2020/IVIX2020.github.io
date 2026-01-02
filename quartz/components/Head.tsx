49b73f01534d6ca5477cbcc0c6eb9c7ca480630a
IVIX <1610frms2020@gmail.com>
2026-01-02 19:06:09 +0900

    Quartz sync: Jan 2, 2026, 7:06 PM

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -42,6 +42,8 @@ export default (() => {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:locale" content="ja_JP" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"></meta>
         <title>{title}</title>
         {cfg.theme.cdnCaching && cfg.theme.fontOrigin === "googleFonts" && (
           <>
